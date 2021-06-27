411b783825ac8bb469864d00f9c473675db2e359
sergioromeroo <manusergio96@gmail.com>
2021-06-27 13:15:30 -0300

    agrego

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+app.use(express.static("public"))
+app.get("/", (req,res) => res.sendFile(path.join(__dirname, "views", "home.html")));
+
+app.listen(port, () => console.log(" en el puerto " + port));
